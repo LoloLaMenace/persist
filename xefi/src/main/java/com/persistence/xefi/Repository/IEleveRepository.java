67cb5429d63bd83cdada80baf2d43cf21c08884b
Lorick <lorick.vacher17@gmail.com>
2024-07-04 14:44:59 +0200

    interface eleve

---
+++ xefi/src/main/java/com/persistence/xefi/Repository/IEleveRepository.java
@@ -0,0 +1,8 @@
+package com.persistence.xefi.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.persistence.xefi.Models.*;
+
+public interface IEleveRepository extends JpaRepository<Eleve, Long> {
+
+}
